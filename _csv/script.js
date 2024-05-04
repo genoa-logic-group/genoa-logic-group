cbd5f4cc4aa903924d96e390a3a9ce98fa6bd784
Fra <fra@Mini-di-Casa.homenet.telecomitalia.it>
2024-05-04 11:10:00 +0200

    update scripts.js

--- _csv/script.js
+++ _csv/script.js
@@ -3,21 +3,30 @@ const parse = require('csv-parse/sync').parse
 
 const input = 'participants.csv' 
 const output = 'participants.yml' 
+const emails = 'emails.txt' 
 const enc = 'utf8'
 
+const invited = ['Awodey','Maietti','Moggi']
+
 let opt = { 
- columns: true, 
+ columns: hd => ['timestamp','email','name','surname','affiliation','website',undefined], 
   skip_empty_lines: true
 } 
 
 let data = parse(fs.readFileSync(input,enc), opt) 
 
 fs.writeFileSync(output, "") 
+fs.writeFileSync(emails, "") 
 
 data.forEach( item => { 
-  fs.appendFileSync(output, `- name: ${item.Name}\n`)
-  fs.appendFileSync(output, `  surname: ${item.Surname}\n`)
-  fs.appendFileSync(output, `  affiliation: ${item.Affiliation}\n`)
+  if (!invited.includes(item.surname)) { 
+    fs.appendFileSync(emails,`${item.email},\n`) 
+    if (item.website) { 
+      fs.appendFileSync(output, `- name: ${item.name}\n`)
+      fs.appendFileSync(output, `  surname: ${item.surname}\n`)
+      fs.appendFileSync(output, `  affiliation: ${item.affiliation}\n`)
+    } 
+  } 
 }) 
 
 
